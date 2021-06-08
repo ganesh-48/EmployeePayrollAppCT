54f8275fea0c66ae0cf78b8899547f49d002156f
ganesh-48 <ganeshsrananavare@gmail.com>
2021-06-08 19:08:00 +0530

    refactor: server file is refactored

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
+require("dotenv").config();
 const express = require('express');
+const dbConfig = require('./config/database.js');
 const logger = require('./config/logger.js');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger-output.json');
-
-// Configuring the database
-const dbConfig = require('./config/database.js');
-require("dotenv").config();
-dbConfig();
+const swaggerDocument = require('./swagger/swagger.json');
 
 // create express app
 const app = express();
+
+// Connect to database
+dbConfig();
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
@@ -17,12 +17,13 @@ app.use(express.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(express.json())
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 // define a simple route
 app.get('/', (req, res) => {
     res.json({ "message": "Welcome to Employee Payroll Application." });
 });
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Require employee payroll routes
 require('./app/routers/employeepayroll.js')(app);
