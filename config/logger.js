b534cf00855aa99f4320769e604077deaded7ff7
ganesh-48 <ganeshsrananavare@gmail.com>
2021-06-08 18:25:58 +0530

    refactor: logger error code is refactored

--- config/logger.js
+++ config/logger.js
@@ -7,6 +7,8 @@ const logger = createLogger({
             level: 'info',
             format: format.combine(format.timestamp(), format.simple())
         }),
+    ],
+    transports: [
         new transports.File({
             filename: 'error.log',
             level: 'error',
