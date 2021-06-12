49354b63c0cc545633261192d28ada356ab19194
ganesh-48 <ganeshsrananavare@gmail.com>
2021-06-12 11:31:12 +0530

    feat: logger error is added

--- config/logger.js
+++ config/logger.js
@@ -5,12 +5,12 @@ const logger = createLogger({
         new transports.File({
             filename: 'logger/info.log',
             level: 'info',
-            format: format.combine(format.timestamp(), format.simple())
+            format: format.combine(format.timestamp(), format.json())
         }),
         new transports.File({
             filename: 'logger/error.log',
             level: 'error',
-            format: format.combine(format.timestamp(), format.simple())
+            format: format.combine(format.timestamp(), format.json())
         }),
     ],
 })
